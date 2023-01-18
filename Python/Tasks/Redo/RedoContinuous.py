c20e9250418bffdfce73ae59e2f3c1eee3329831
antaenc <ant@senzing.com>
2023-01-18 11:01:15 -0500

    Issue #6 - change redo api calls (#7)

--- Python/Tasks/Redo/RedoContinuous.py
+++ Python/Tasks/Redo/RedoContinuous.py
@@ -17,27 +17,28 @@ def mock_logger(level, exception, error_rec=None):
 def process_redo(engine):
     success_recs = 0
 
-    try:
-        while True:
+    while True:
+        try:
             redo_record = bytearray()
-            engine.processRedoRecord(redo_record)
+            engine.getRedoRecord(redo_record)
 
             if not redo_record:
                 print(f'No redo records to process, pausing for 30 seconds. Total processed {success_recs} . (CTRL-C to exit)...')
                 time.sleep(30)
                 continue
 
-            success_recs += 1
+            engine.process(redo_record.decode())
 
+            success_recs += 1
             if success_recs % 100 == 0:
                 print(f'Processed {success_recs} redo records')
-    except G2BadInputException as ex:
-        mock_logger('ERROR', ex)
-    except G2RetryableException as ex:
-        mock_logger('WARN', ex)
-    except (G2UnrecoverableException, G2Exception) as ex:
-        mock_logger('CRITICAL', ex)
-        raise
+        except G2BadInputException as ex:
+            mock_logger('ERROR', ex)
+        except G2RetryableException as ex:
+            mock_logger('WARN', ex)
+        except (G2UnrecoverableException, G2Exception) as ex:
+            mock_logger('CRITICAL', ex)
+            raise
 
 
 try:
